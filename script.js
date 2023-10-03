4e0078a2ee355478f26e607837df094554f0474b
Mysterynisha <106901424+Mysterynisha@users.noreply.github.com>
2023-10-03 17:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
 function getFormvalue() {
-    let x= document.forms['form1'];
-	for(e=0;e<x.length;e++)
-		{
-			txt+=x.elements[e].value;
-		}
-	
+    	
 let first =document.querySelector('#form1 input[name="fname"]');
 let last =document.querySelector('#form1 input[name="lname"]');
 	let fname=first.value;
